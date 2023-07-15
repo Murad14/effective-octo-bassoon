91b6eb0537684324296384fd6812ebd44186f636
Murad <49407545+Murad14@users.noreply.github.com>
2023-07-16 01:50:34 +0600

    modify

--- socialproject/posts/models.py
+++ socialproject/posts/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings
-
+from django.utils.text import slugify
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,
@@ -14,3 +14,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
     
+    def save(self,*args, **kwargs):
+        if not self.slug:
+          self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
