f14a566915e61e1f01d91502ee999133f86d46ce
Murad <49407545+Murad14@users.noreply.github.com>
2023-07-16 11:26:45 +0600

    create-post fuctionality done

--- socialproject/posts/views.py
+++ socialproject/posts/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import PostCreateForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
+def post_create(req):
+    if req.method == 'POST':
+        form = PostCreateForm(data=req.POST,files=req.FILES)
+        if form.is_valid():
+            new_item = form.save(commit=False)
+            new_item.user = req.user
+            new_item.save()
+    else:
+        form = PostCreateForm(data=req.GET)
+        
+    return render(req,'posts/create.html',{'form':form})
