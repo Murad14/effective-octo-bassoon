f6dd0d19c5a89eebb981694ff53b5a779763f4c0
Murad <49407545+Murad14@users.noreply.github.com>
2023-08-25 15:19:36 +0600

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
+import LoginPage from "./pages/LoginPage";
 import RegisterPage from './pages/RegisterPage';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div className="App">
+      <ToastContainer></ToastContainer>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<HomePage/>}></Route>
-          <Route path='/login' element={<LoginPage/>}></Route>
-          <Route path='/register' element={<RegisterPage/>}></Route>
+          <Route path='/home' element={<HomePage />}></Route>
+          <Route path='/login' element={<LoginPage />}></Route>
+          <Route path='/register' element={<RegisterPage />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+export default function LoginPage() {
+
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-100">
+            <div className="w-full max-w-md p-6 bg-white rounded shadow-lg">
+                <h1 className="text-2xl mb-6">🧔 Login User</h1>
+                <form>
+                    <div className="mb-4">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                            Username
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="username"
+                            type="text"
+                            placeholder="Username"
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="password"
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </div>
+                    <div className="flex items-center justify-center">
+                        <Link
+                            to="/home"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        >
+                            Log In
+                        </Link>
+                        <Link
+                            to="/register"
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline ml-2"
+                        >
+                            Sign Up
+                        </Link>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
