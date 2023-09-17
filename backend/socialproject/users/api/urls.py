c36df778a3b81a67ab894690803843c90b232545
Murad <49407545+Murad14@users.noreply.github.com>
2023-09-18 02:02:45 +0600

    modified

--- backend/socialproject/users/api/urls.py
+++ backend/socialproject/users/api/urls.py
@@ -1,17 +1,22 @@
 from django.urls import path
 from . import views
 
-from .views import MyTokenObtainPairView
-
 from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
     TokenRefreshView,
+    
 )
 
+from .views import RegisterAPIView
 
 
 urlpatterns = [
-    path ('', views.getRoutes),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    #path ('', views.getRoutes),
+    path('login/', TokenObtainPairView.as_view(), name='login_view'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh_view'),
+    
+    path('register/', RegisterAPIView.as_view(), name='register_view'),
     
 ]
+
+
--- backend/socialproject/users/api/views.py
+++ backend/socialproject/users/api/views.py
@@ -1,7 +1,31 @@
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from .serializers import UserRegisterSerializer
+from rest_framework import status, serializers
 
+
+class RegisterAPIView(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+'''
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -29,3 +53,4 @@ def getRoutes(request):
     ]
     
     return Response(routes) 
+'''
