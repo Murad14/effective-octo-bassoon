c2ee5cbc1de3a64521fb5c5481450da25cf28c0d
Murad <49407545+Murad14@users.noreply.github.com>
2023-09-26 22:18:01 +0600

    profile model created

--- backend/socialproject/users/models.py
+++ backend/socialproject/users/models.py
@@ -3,6 +3,6 @@ from .api.models import Profile
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # Assuming you have a User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)  
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    # Other fields specific to your UserProfile model
+  
